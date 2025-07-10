f363dad0f231e0f4169551b8f931a0cf20f76311
tomas <tomas27@gmail.com>
2025-07-09 19:03:22 -0600

    segundo commid tomas

--- farmacia.py
+++ farmacia.py
@@ -1,8 +1,9 @@
 
 
-def Mostar_menu():
-    print("Farmacia Menu")
-    print("1. Agregara medicamentos a la pila")
-    print("2. Entregar mediacamentos")
-    print("3. Mostrar")
-    print("4. Salir")
+print("Farmacia Menu")
+print("1. Agregara medicamentos a la pila")
+print("2. Entregar mediacamentos")
+print("3. Mostrar")
+print("4. Salir")
+print("Ingrese su opcion ")
+pila_medicamentos = []
