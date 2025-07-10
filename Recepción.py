3cef14c38bffa8e9fd0f914cf150e11d289cc4a7
Rudy <rudyax2004@gmail.com>
2025-07-09 19:13:46 -0600

    cuarto commit Rudy

--- Recepción.py
+++ Recepción.py
@@ -6,18 +6,31 @@ print("1.- Registrar un nuevo paciente")
 print("2.- Atender al paciente")
 print("3.- Mostrar pacientes en cola")
 print("4.- Salir")
-opcion = input("Seleccione una opción: ")
+while True:
+    opcion = input("Seleccione una opción: ")
 
-if opcion == "1":
-    Paciente = input("Nombre del paciente que va registrar: ")
-    cola_Pacientes.append(Paciente)
-    print(f"el paciente {Paciente} se agrego a la cola")
-elif opcion == "2":
-    if cola_Pacientes:
-        Atendido = cola_Pacientes.pop()
-        print(f"el paciente {Atendido} fue atendido con éxito")
+    if opcion == "1":
+        Paciente = input("Nombre del paciente que va registrar: ")
+        cola_Pacientes.append(Paciente)
+        print(f"el paciente {Paciente} se agrego a la cola")
+    elif opcion == "2":
+        if cola_Pacientes:
+            Atendido = cola_Pacientes.pop()
+            print(f"el paciente {Atendido} fue atendido con éxito")
+        else:
+            print("No se ha registrado ningún paciente")
+    elif opcion == "3":
+        if cola_Pacientes:
+            print ("La cola Acutal de pacientes es de: ")
+            for Paciente in cola_Pacientes:
+                print(Paciente)
+        else:
+            print("No hay ningun paciente")
+    elif opcion == "4":
+        print("Gracias por utilizar nuestro programa")
+        break
     else:
-        print("No se ha registrado ningún paciente")
+        print("Opcion No encontrada o no valida.")
 
 
 
