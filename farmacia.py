a0e5709f671d1d151e5b6d03e73ab678158745b4
tomas <tomas27@gmail.com>
2025-07-09 19:08:17 -0600

    tercer commid tomas

--- farmacia.py
+++ farmacia.py
@@ -5,5 +5,11 @@ print("1. Agregara medicamentos a la pila")
 print("2. Entregar mediacamentos")
 print("3. Mostrar")
 print("4. Salir")
-print("Ingrese su opcion ")
+opcion = input("Ingrese su opción ")
 pila_medicamentos = []
+if opcion == "1":
+    medicamento = input("Ingrese el medicamento a agregar: ")
+    pila_medicamentos.append(medicamento)
+    print(f"{medicamento} ingresado corectamente")
+
+elif
