2f3d8b78cdd969e2dd9f108f2a51383a76fd32f6
tomas <tomas27@gmail.com>
2025-07-09 19:13:49 -0600

    cuarto commid tomas

--- farmacia.py
+++ farmacia.py
@@ -5,11 +5,16 @@ print("1. Agregara medicamentos a la pila")
 print("2. Entregar mediacamentos")
 print("3. Mostrar")
 print("4. Salir")
-opcion = input("Ingrese su opción ")
-pila_medicamentos = []
-if opcion == "1":
-    medicamento = input("Ingrese el medicamento a agregar: ")
-    pila_medicamentos.append(medicamento)
-    print(f"{medicamento} ingresado corectamente")
 
-elif
+while True:
+    opcion = input("Ingrese su opción ")
+    pila_medicamentos = []
+    if opcion == "1":
+        medicamento = input("Ingrese el medicamento a agregar: ")
+        pila_medicamentos.append(medicamento)
+        print(f"{medicamento} ingresado corectamente")
+
+    elif opcion==2:
+        if pila_medicamentos:
+        entregado = pila_medicamentos.pop()
+        print(f"Medicamento entregado {entregado}")
