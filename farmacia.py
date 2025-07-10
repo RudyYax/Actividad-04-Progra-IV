73b1e838276635a3146a3c61ca2ae4680f957c2d
tomas <tomas27@gmail.com>
2025-07-09 18:58:07 -0600

    primer commid tomas

--- farmacia.py
+++ farmacia.py
@@ -1,3 +1,8 @@
 
 
-print("Que onda")
+def Mostar_menu():
+    print("Farmacia Menu")
+    print("1. Agregara medicamentos a la pila")
+    print("2. Entregar mediacamentos")
+    print("3. Mostrar")
+    print("4. Salir")
