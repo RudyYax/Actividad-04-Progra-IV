e095a14a6e6b4205aba2523b4667151a8a7e615b
Rudy <rudyax2004@gmail.com>
2025-07-09 18:57:44 -0600

    Primer commit Rudy

--- Recepción.py
+++ Recepción.py
@@ -1,2 +1,13 @@
+cola_Pacientes = []
+opcion = 0
 print("Bienvenido a Recepcion")
 print("A continuacion presentare un breve menu puede seleccionar la opcion que desee")
+print("1.- Registrar un nuevo paciente")
+print("2.- Atender al paciente")
+print("3.- Mostrar pacientes en cola")
+print("4.- Salir")
+
+if opcion == 1:
+    print("Registrar un nuevo paciente")
+    print()
+
